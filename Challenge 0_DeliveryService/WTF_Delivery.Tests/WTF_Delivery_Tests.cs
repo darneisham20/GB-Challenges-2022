f226ff7e7f92668b7d8d154b5d7f3f5cdd697905
Darneisha Miller <darneisham20@gmail.com>
2022-11-26 11:52:05 -0500

    Deliver Service Tests

--- Challenge 0_DeliveryService/WTF_Delivery.Tests/WTF_Delivery_Tests.cs
+++ Challenge 0_DeliveryService/WTF_Delivery.Tests/WTF_Delivery_Tests.cs
@@ -3,8 +3,25 @@ namespace WTF_Delivery.Tests;
 public class WTF_Delivery_Tests
 {
     [Fact]
-    public void Test1()
+    public void SetCorrectOrderNumber()
     {
+        ServiceContent content = new ServiceContent();
+        content.OrderNumber = "111";
 
+        string expected = "111";
+        string actual = content.OrderNumber;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    public void SetCorrectItemName()
+    {
+        ServiceContent content = new ServiceContent();
+        content.ItemName = "Macintosh Laptop Charger";
+
+        string expected = "Macintosh Laptop Charger";
+        string actual = content.ItemName;
+
+        Assert.AreEqual(expected, actual);
     }
 }
